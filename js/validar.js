a9c7d47631edabec2e7423d9af95a51b1f4a296c
julio-arano <146580025+julio-arano@users.noreply.github.com>
2023-10-26 13:04:58 -0300

    Update validar.js

--- js/validar.js
+++ js/validar.js
@@ -1,18 +1,35 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>validacion</title>
-  <script src="./js/validar.js"></script>
-</head>
-<body>
-  <form name='f1' onsubmit="return validar();">
-      <label for="usuario">Usuario</label>  
-    <input type='text' name='usuario' id='usuario' value="" />
-    <div id="validar_usuario" style="color:red;"></div>
-    <label for="clave">Clave: (longitud>=8 caracteres)</label>  
-    <input type='text' name='clave' id='clave' value="" />
-    <div id="validar_clave" style="color:red;"></div>
-      <input type='submit'  value='Enviar'></td>
-  </form>
-</body>
-</html>
+function validar() {
+    let usuario = document.getElementById("usuario");
+    let clave = document.getElementById("clave");
+    let error = false;
+document.getElementById("validar_usuario").innerHTML = "";
+document.getElementById("validar_clave").innerHTML = "";    
+if (usuario.value == "") {
+        document.getElementById("validar_usuario").innerHTML = "Debe completar el usuario";
+        error = true;
+        usuario.focus();
+        error = true
+    }
+    if (clave.value.length<8) {
+        document.getElementById("validar_clave").innerHTML = "Debe completar el clave con 8 carateres como minimo";
+        error = true;
+        clave.focus();
+        error = true
+    }
+ 
+    if (error == false) {
+        alert("Dato enviado")
+        document.getElementById("usuario").value = ""
+        document.getElementById("validar_usuario").innerHTML = "";
+        document.getElementById("clave").value = ""
+        document.getElementById("validar_clave").innerHTML = "";
+    }
+
+
+
+
+    if (error)
+        return false;
+    else
+        return true;
+}
