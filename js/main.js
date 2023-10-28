477060daf10db198e88f835e16fd3ec363c91b90
babalubah <145021928+babalubah@users.noreply.github.com>
2023-10-28 20:07:49 -0300

    puse el header y el footer desde Main.js en todas las secciones

--- js/main.js
+++ js/main.js
@@ -25,3 +25,25 @@ createApp({
        }
    
 }).mount('#app')
+let cad=` 
+        <div>
+        <nav>    <!--  nav a las otras paginas -->
+            <a href="index.html">Home</a>
+            <a href="./hospedajes.html">¿Dónde dormir?</a>
+            <a href="./gastronomia.html">¿Dónde comer?</a>
+            <a href="./turismo.html">¿Qué hacer?</a>
+            <a href="./fiestapop.html">Fiesta del Pastel!</a>
+            <a href="./contactos.html">Contacto</a>
+            <a href="./mapa.html">¿Cómo llegar?</a>
+            <a href="./condiciones.html">Condiciones TPO</a>
+            <a href="https://www.carmendeareco.gob.ar/turismo/">Web oficial<img src="./img/logo_Carmen-de-Areco.png" alt=""width="50" height=""></a>     
+        </nav>
+        </div>
+` 
+document.querySelector("header").innerHTML=cad
+cad=`
+<div class="col-12 col-s-12 menu" >
+       <p> Proyecto CAC 23537 - Blumberg/Dalmao/Arano - 2023</p>
+      </div>
+`
+document.querySelector("footer").innerHTML=cad
